80783ce451f765384d74f4b38227e214f449d629
Norfirion <Norfirionszt@gmail.com>
2021-03-10 14:36:00 +0100

    javascript

---
+++ public_html/szamologep.js
@@ -0,0 +1,45 @@
+var valtA="";
+var valtB="";
+var valtM="";
+
+
+function osszead(){
+    var osszeg=Number(valtA)+Number(valtB); 
+    document.getElementById("szoveg").innerHTML=osszeg;
+}
+function kivonas(){
+    var osszeg=Number(valtA)-Number(valtB); 
+    document.getElementById("szoveg").innerHTML=osszeg;
+}
+function osztas(){
+    var osszeg=Number(valtA)/Number(valtB);
+    document.getElementById("szoveg").innerHTML=osszeg;
+}
+function szorzas(){
+    var osszeg=Number(valtA)*Number(valtB); 
+    document.getElementById("szoveg").innerHTML=osszeg;
+}
+
+function szamolas(){
+
+valtA=document.getElementById("a").value;
+valtB=document.getElementById("b").value;
+valtM=document.getElementById("muv").value;
+if(valtM === "+"){
+    osszead();    
+}
+if(valtM === "-"){
+    kivonas();   
+}
+if(valtM === "*"){
+    szorzas();    
+}
+if(valtM === "-"){
+    osztas();  
+}
+
+var osszeg=Number(valtA)+Number(valtB);       
+
+
+
+}
